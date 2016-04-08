a7c1e9fe2001ff9177e0461d23f9aaafe8152519
Yuriy Shestakov <yshestakov@gmail.com>
2016-04-08 23:40:17 +0300

    Added some stub implementation for GUI5/zad1

---
+++ GUI5_SD_S14145/src/Spiewak.java
@@ -0,0 +1,21 @@
+/**
+ *
+ *  @author Shevchenko Dariia S14145
+ *
+ */
+
+package zad1;
+
+
+public abstract class Spiewak {
+    private String name;
+    private String song;
+
+    public Spiewak(String name) {
+        this.name = name;
+    }
+
+    static public String najglosniej(Spiewak[] sp) {
+        return "Not Implemented";
+    }
+}

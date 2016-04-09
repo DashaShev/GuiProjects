f44acdcabff9b85fd22d11c3cc22b673c2ba76cb
DashaShev <sdasha321@gmail.com>
2016-04-09 18:11:50 +0200

    Update Spiewak.java

--- GUI5_SD_S14145/src/zad1/Spiewak.java
+++ GUI5_SD_S14145/src/zad1/Spiewak.java
@@ -10,12 +10,21 @@ package zad1;
 public abstract class Spiewak {
     private String name;
     private String song;
+    public int numer = 0;
 
     public Spiewak(String name) {
         this.name = name;
+        numer++;
+         
+        
     }
 
     static public String najglosniej(Spiewak[] sp) {
         return "Not Implemented";
+    }
+    
+    public String toString(){
+		return "(" + numer + ") " + name + ": " + song;
+    	
     }
 }

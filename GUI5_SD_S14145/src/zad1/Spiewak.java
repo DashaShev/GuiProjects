48a9d63e6d77be988bbafd0e46a8115004389a5e
Yuriy Shestakov <yshestakov@gmail.com>
2016-04-09 19:23:04 +0300

    Added sp_index / my_num into the Spiewak class

--- GUI5_SD_S14145/src/zad1/Spiewak.java
+++ GUI5_SD_S14145/src/zad1/Spiewak.java
@@ -8,11 +8,15 @@ package zad1;
 
 
 public abstract class Spiewak {
+    static Integer sp_index = 0;
+    private Integer my_num;
     private String name;
     private String song;
 
     public Spiewak(String name) {
         this.name = name;
+        my_num += 1;
+        this.sp_index = my_num;
     }
 
     static public String najglosniej(Spiewak[] sp) {

6c6cfa3ca4fd52f7a8b70acdc10df35964df920c
DashaShev <sdasha321@gmail.com>
2016-04-09 20:06:19 +0200

    Update Spiewak.java

--- GUI5_SD_S14145/src/zad1/Spiewak.java
+++ GUI5_SD_S14145/src/zad1/Spiewak.java
@@ -20,18 +20,28 @@ public abstract class Spiewak {
     abstract String spiewaj();
 
     static public String najglosniej(Spiewak[] sp) {
-    	
+    	int tab [] = new int [sp.length];
+    	int k = 0;
     	for (Spiewak s : sp) {int count = 0; 
     		String spiew = s.spiewaj(); 
+    		
     		for (int i = 0; i < spiew.length(); i++){
     			if(spiew.charAt(i) >= 'A' && spiew.charAt(i) <= 'Z'){
     				count++;
-    			} 
-    			int ptr = count; 
-    			if(count < ptr) 
+    			}
     		
-    		
-    		} //System.out.println(count);	
+    		 tab[k] = count; 
+    		 k++; 
+    		} 
+    		int max = tab[0];
+    		for (int l = 0; l < tab.length; l++){
+    			if (max < tab[l])
+                {
+                    max = tab[l];
+                } 
+    		}
+    		//System.out.println(max);
+    		//System.out.println(count);	
     		
     	}
 		return null;

2412dbcae89cceaa18fb53a3e7a53f088533502b
DashaShev <sdasha321@gmail.com>
2016-04-15 20:45:55 +0200

    Update ReversibleString.java

--- GUI5_SD_S14145/src/zad2/ReversibleString.java
+++ GUI5_SD_S14145/src/zad2/ReversibleString.java
@@ -1,10 +1,16 @@
 package zad2;
 
 public class ReversibleString implements Reversible {
+	private String slowo;
+
+	public ReversibleString(String string) {
+		this.slowo = string; 
+	}
 
 	@Override
 	public void reverse() {
 		
+
 	}
 
 }

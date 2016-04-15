9d13e429e5785a7dccc133f12bc9f931829f4fa6
DashaShev <sdasha321@gmail.com>
2016-04-15 21:03:12 +0200

    Update ReversibleString.java

--- GUI5_SD_S14145/src/zad2/ReversibleString.java
+++ GUI5_SD_S14145/src/zad2/ReversibleString.java
@@ -2,15 +2,24 @@ package zad2;
 
 public class ReversibleString implements Reversible {
 	private String slowo;
-
+	private int count = 0;
+	
 	public ReversibleString(String string) {
 		this.slowo = string; 
 	}
 
 	@Override
-	public void reverse() {
+	public String reverse() {
+		if (count == 0) {
+	        String newString = new StringBuffer(slowo).reverse().toString();
+			return newString;
+			count++; 
+		}
+		if (count == 1){
+			return "Tekst " + slowo; 
+		}
+	
 		
-
 	}
 
 }

0d03f00ed59aab78c62d567a3f51077f2173c1b0
DashaShev <sdasha321@gmail.com>
2016-04-15 20:46:23 +0200

    Update ReversibleDouble.java

--- GUI5_SD_S14145/src/zad2/ReversibleDouble.java
+++ GUI5_SD_S14145/src/zad2/ReversibleDouble.java
@@ -1,9 +1,14 @@
 package zad2;
 
 public class ReversibleDouble implements Reversible {
+	private int cyfra;
+
+	public ReversibleDouble(int i) {
+		this.cyfra = i; 
+	}
 
 	@Override
-	public void reverse() {
+	public double reverse() {
 		
 
 	}

762325e12640702bb330138668d85303ad7fbe9e
DashaShev <sdasha321@gmail.com>
2016-04-16 21:59:21 +0200

    Added files via upload

---
+++ GUI6_SD_S14145/src/zad2/OpAdd.java
@@ -0,0 +1,11 @@
+package zad2;
+
+public class OpAdd implements MathOp{
+
+	@Override
+	public Float op(Float n1, Float n2) {
+		
+		return n1 + n2; 
+	}
+
+}
---
+++ GUI6_SD_S14145/src/zad2/OpMul.java
@@ -0,0 +1,11 @@
+package zad2;
+
+public class OpMul implements MathOp{
+
+	@Override
+	public Float op(Float n1, Float n2) {
+		
+		return n1 * n2; 
+	}
+
+}

9e3754f3602ed609e0042616f194ff31ed08c934
DashaShev <sdasha321@gmail.com>
2016-04-22 18:20:42 +0200

    Update Letters.java

--- GUI7_SD_S14145/src/zad1/Letters.java
+++ GUI7_SD_S14145/src/zad1/Letters.java
@@ -2,10 +2,23 @@ package zad1;
 
 public class Letters {
 
+	private String letters;
+	
 	public Letters (String letters){
-		
+		this.letters = letters; 
 	}
 
-	
+	 Runnable r1 = new Runnable() {
+	      @Override
+	      public void run() {
+	       
+	    	  for (int i = 0; i < letters.length(); i++){
+	    		  System.out.println("Thread" + letters.charAt(i));
+	    	  }
+	    	  
+	    };
+	    
+	    
+	 };
 	
 }

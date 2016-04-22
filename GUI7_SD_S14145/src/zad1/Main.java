f4d190353f28b1530e71ca695045d9743e655591
DashaShev <sdasha321@gmail.com>
2016-04-22 20:57:08 +0200

    Update Main.java

--- GUI7_SD_S14145/src/zad1/Main.java
+++ GUI7_SD_S14145/src/zad1/Main.java
@@ -12,16 +12,15 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
     Letters letters = new Letters("ABCD");
     for (Thread t : letters.getThreads()) System.out.println(t.getName());
-    /*<- tu uruchomić 
-         wszystkie kody w wątkach 
-     */
+    
+    for (Thread t : letters.getThreads()) t.start();
 
     Thread.sleep(5000);
 
-    /*<- tu trzeba zapisać
-       fragment, który kończy działanie kodów, wypisujących litery 
+    /*<- tu trzeba zapisaД‡
+       fragment, ktГіry koЕ„czy dziaЕ‚anie kodГіw, wypisujД…cych litery 
     */
-    System.out.println("\nProgram skończył działanie");
+    System.out.println("\nProgram skoЕ„czyЕ‚ dziaЕ‚anie");
   }
 
 }

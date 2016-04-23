f5aed1424e2d3a9c01feb5590a2d218121a0f65a
DashaShev <sdasha321@gmail.com>
2016-04-23 17:46:12 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	GUI7_SD_S14145/src/zad1/Main.java
    
    7

---
+++ GUI7_SD_S14145/src/zad2/StringTask.java
@@ -0,0 +1,40 @@
+package zad2;
+
+public class StringTask implements Runnable{
+	
+	public StringTask(String letter, int count){
+		
+	}
+
+	public String getResult() {
+		
+		return null;
+	}
+
+	public TaskState getState() {
+		
+		return null;
+	}
+
+	public void start() {
+		
+		
+	}
+	
+	public void abort() {
+		
+		
+	}
+
+	public boolean isDone() {
+		
+		return false;
+	}
+
+	@Override
+	public void run() {
+		
+		
+	}
+
+}

bd779a02f51aeaf6500cbcc977d6a6466764e0e1
DashaShev <sdasha321@gmail.com>
2016-04-30 22:41:19 +0200

    Update StringTask.java

--- GUI7_SD_S14145/src/zad2/StringTask.java
+++ GUI7_SD_S14145/src/zad2/StringTask.java
@@ -16,22 +16,16 @@ public class StringTask implements Runnable {
 		this.task_state = TaskState.CREATED;
 		this.res = new String();
 	}
-
-	public void debug(String msg)
-	{
-	   System.out.format("%d: %s\n", System.currentTimeMillis(), msg);
-	}
 	
 	@Override
     public void run() {
-		debug("ENTER run()");
         this.task_state = TaskState.RUNNING;
 		while (task_state != TaskState.ABORTED && this.cnt < this.iter_num) {
 		 	res = res + letter;
 		    this.cnt += 1;
 		}
 	    task_state = TaskState.READY;
-		debug("LEAVE run()");
+
     }
 
 
@@ -49,18 +43,19 @@ public class StringTask implements Runnable {
 	}
 	
 	public void abort() {
+		task_state = TaskState.ABORTED;
 		my_thr.interrupt();
 		try {
 			my_thr.join();
-			task_state = TaskState.ABORTED;
+			
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
 
 	public boolean isDone() {
-		return  task_state == TaskState.READY;
-	}
+		  return  task_state == TaskState.READY || task_state == TaskState.ABORTED;
+		 }
 
 }

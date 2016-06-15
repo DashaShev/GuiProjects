8ba84e5a237ed1011b52ee14ff6f9bef256ab36d
Yuriy Shestakov <yshestakov@gmail.com>
2016-06-15 22:45:31 +0300

    Added task1/JListTempConv.java

---
+++ task1/JListTempConv.java
@@ -0,0 +1,35 @@
+package task1;
+import javax.swing.JList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPanel;
+import javax.swing.AbstractListModel;
+
+
+
+public class JListTempConv {
+
+    public static void main(String args[]){
+        JFrame listFrame = new JFrame();
+        JPanel contentPane = (JPanel) listFrame.getContentPane();
+        JList list = new JList<String>(new AbstractListModel<String>() {
+            Integer min_c_degree = -70;
+            Integer max_c_degree = +60;
+            @Override
+            public int getSize() {
+                return max_c_degree - min_c_degree;
+            }
+
+            @Override
+            public String getElementAt(int index) {
+                Integer c_degree = index-70;
+                float f_degree = c_degree * 1.8f + 32.0f;
+                return String.format("%d degree C = %.1f degree F", c_degree, f_degree);
+            }
+        });
+        contentPane.add(list);
+        listFrame.pack();
+        listFrame.setVisible(true);
+        listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}

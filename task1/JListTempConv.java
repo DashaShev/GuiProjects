837342759bd3480d39eb265174008935278ed1a0
Yuriy Shestakov <yshestakov@gmail.com>
2016-06-15 22:50:48 +0300

    Modified to use JScrollPane

--- task1/JListTempConv.java
+++ task1/JListTempConv.java
@@ -2,7 +2,7 @@ package task1;
 import javax.swing.JList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.AbstractListModel;
 
 
@@ -12,6 +12,7 @@ public class JListTempConv {
     public static void main(String args[]){
         JFrame listFrame = new JFrame();
         JPanel contentPane = (JPanel) listFrame.getContentPane();
+
         JList list = new JList<String>(new AbstractListModel<String>() {
             Integer min_c_degree = -70;
             Integer max_c_degree = +60;
@@ -27,7 +28,10 @@ public class JListTempConv {
                 return String.format("%d degree C = %.1f degree F", c_degree, f_degree);
             }
         });
-        contentPane.add(list);
+        // contentPane.add(list);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.getViewport().setView(list);
+        contentPane.add(scrollPane);
         listFrame.pack();
         listFrame.setVisible(true);
         listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
